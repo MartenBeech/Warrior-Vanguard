198843e2f1d26ef62aec64e78fa19bcf6ee5e106
Morten Bech <mortenbech22@hotmail.com>
2025-02-22 21:15:27 +0100

    Added player deck with onClick handler

--- Assets/Scripts/Battlefield/Deck.cs
+++ Assets/Scripts/Battlefield/Deck.cs
@@ -6,5 +6,16 @@ using UnityEngine.UI;
 
 public class Deck : MonoBehaviour
 {
-    
+    public GameObject deck;
+    public GameObject text;
+    int deckSize = 50;
+
+    public void DrawCard() {
+        deckSize--;
+        UpdateDeckUi();
+    }
+
+    void UpdateDeckUi() {
+        text.GetComponent<TMPro.TextMeshProUGUI>().text = $"{deckSize}";
+    }
 }
