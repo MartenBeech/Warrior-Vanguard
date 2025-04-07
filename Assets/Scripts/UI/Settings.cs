a6f9e3858a3e0c0f074dd905310ca521b2d2b2b1
Morten Bech <mortenbech22@hotmail.com>
2025-04-07 22:11:36 +0200

    Updated settings to carryover through scenes

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -1,58 +1,16 @@
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class Settings : MonoBehaviour {
-    public GameObject settingsObject;
-    public TextMeshProUGUI gameSpeedText;
     public static int gameSpeed = 4;
-    int musicVolumePercentage = 0;
+    public static int musicVolumePercentage = 0;
     public MusicPlayer musicPlayer;
-    public GameObject musicPlayerMute;
-    public GameObject[] musicPlayerBars;
-    public Sprite audioIcon;
-    public Sprite audioIconMute;
 
     void Start() {
-        gameSpeedText.text = $"{gameSpeed}";
         UpdateMusicVolume(musicVolumePercentage);
     }
 
-    public void ToggleSettingsEnabled() {
-        if (settingsObject.activeSelf) {
-            settingsObject.SetActive(false);
-        } else {
-            settingsObject.SetActive(true);
-        }
-    }
-
-    public void IncreaseGameSpeed() {
-        if (gameSpeed >= 4) return;
-        gameSpeed *= 2;
-        gameSpeedText.text = $"{gameSpeed}";
-    }
-
-    public void DecreaseGameSpeed() {
-        if (gameSpeed <= 1) return;
-        gameSpeed /= 2;
-        gameSpeedText.text = $"{gameSpeed}";
-    }
-
     public void UpdateMusicVolume(int volumePercentage) {
+        musicVolumePercentage = volumePercentage;
         if (musicPlayer) musicPlayer.UpdateVolume(volumePercentage);
-
-        if (volumePercentage > 0) {
-            musicPlayerMute.GetComponent<Image>().sprite = audioIcon;
-        } else {
-            musicPlayerMute.GetComponent<Image>().sprite = audioIconMute;
-        }
-
-        for (int i = 0; i < 5; i++) {
-            if (volumePercentage >= (i * 20) + 20) {
-                musicPlayerBars[i].GetComponent<Image>().color = ColorPalette.GetColor(ColorPalette.ColorEnum.black);
-            } else {
-                musicPlayerBars[i].GetComponent<Image>().color = ColorPalette.GetColor(ColorPalette.ColorEnum.gray);
-            }
-        }
     }
 }
---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour {
+    public GameObject settingsObject;
+    public TextMeshProUGUI gameSpeedText;
+    public GameObject musicPlayerMute;
+    public GameObject[] musicPlayerBars;
+    public Sprite audioIcon;
+    public Sprite audioIconMute;
+    public Settings settings;
+
+    void Start() {
+        gameSpeedText.text = $"{Settings.gameSpeed}";
+        UpdateMusicVolume(Settings.musicVolumePercentage);
+    }
+
+    public void ToggleSettingsEnabled() {
+        if (settingsObject.activeSelf) {
+            settingsObject.SetActive(false);
+        } else {
+            settingsObject.SetActive(true);
+        }
+    }
+
+    public void IncreaseGameSpeed() {
+        if (Settings.gameSpeed >= 4) return;
+        Settings.gameSpeed *= 2;
+        gameSpeedText.text = $"{Settings.gameSpeed}";
+    }
+
+    public void DecreaseGameSpeed() {
+        if (Settings.gameSpeed <= 1) return;
+        Settings.gameSpeed /= 2;
+        gameSpeedText.text = $"{Settings.gameSpeed}";
+    }
+
+    public void UpdateMusicVolume(int volumePercentage) {
+        settings.UpdateMusicVolume(volumePercentage);
+
+        if (volumePercentage > 0) {
+            musicPlayerMute.GetComponent<Image>().sprite = audioIcon;
+        } else {
+            musicPlayerMute.GetComponent<Image>().sprite = audioIconMute;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (volumePercentage >= (i * 20) + 20) {
+                musicPlayerBars[i].GetComponent<Image>().color = ColorPalette.GetColor(ColorPalette.ColorEnum.black);
+            } else {
+                musicPlayerBars[i].GetComponent<Image>().color = ColorPalette.GetColor(ColorPalette.ColorEnum.gray);
+            }
+        }
+    }
+}
