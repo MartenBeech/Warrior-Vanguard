ae048accc6f82f64f70f2c59af000379cc431bc9
Bechy7 <soeren-bechy7@hotmail.com>
2025-08-06 12:43:33 +0200

    Changed GainMaxHealth event to gain Accept button

--- Assets/Scripts/Event/Events/GainMaxHealth.cs
+++ Assets/Scripts/Event/Events/GainMaxHealth.cs
@@ -3,10 +3,15 @@ public class GainMaxHealth {
     public Event GetEvent(EventManager eventManager) {
         Event newEvent = new() {
             OnSetup = () => {
-                int healthDifference = 5;
-                eventManager.eventText.text = $"A stranger walks up to you and gives you a potion. Without hesitation, you drink it and gains {healthDifference} max health. You are happy about your life choices.";
-                eventManager.summonerManager.GainMaxHealth(healthDifference);
+                eventManager.eventText.text = "A stranger walks up to you and gives you a potion. Do you drink it?";
+                
             },
+
+            OnAccept = () => {
+                int healthDifference = 5;
+                eventManager.summonerManager.GainMaxHealth(healthDifference);
+                eventManager.eventText.text = $"Without hesitation, you drink the stranger's potion and gains {healthDifference} max health. You are happy about your life choices.";
+            }
         };
 
         return newEvent;
