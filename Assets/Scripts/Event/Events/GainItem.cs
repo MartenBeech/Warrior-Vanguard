ae3ea3ffae5b682cbadbbe08eab5482b74f4a00b
Bechy7 <soeren-bechy7@hotmail.com>
2025-08-28 09:07:36 +0200

    Changed the UI for Item-Event

--- Assets/Scripts/Event/Events/GainItem.cs
+++ Assets/Scripts/Event/Events/GainItem.cs
@@ -12,6 +12,8 @@ public class GainItem {
                     PlayerPrefs.SetString(eventManager.eventGainItemKey, eventManager.item.title);
                     PlayerPrefs.Save();
                 }
+                eventManager.option2Text.text = "Pick up the item";
+                eventManager.option1Text.text = "Leave it alone";
 
                 if (eventManager.item.title == null) {
                     eventManager.eventText.text = "You find a hole where an item used to be. However, it seems like you already have all the items in the world.";
@@ -27,12 +29,17 @@ public class GainItem {
                 eventManager.eventText.text = $"You find a mysterious item on the ground called {eventManager.item.displayTitle}. Do you want to pick it up?";
             },
 
-            OnClickOption1 = () => {
+            OnClickOption2 = () => {
                 eventManager.item.UseImmediately(new(summoner: eventManager.summonerManager.summoner));
                 ItemManager.AddItem(eventManager.item);
                 eventManager.eventText.text = $"You picked up {eventManager.item.displayTitle}!";
                 eventManager.itemRewardPanel.SetActive(false);
-            }
+            },
+
+            OnClickOption1 = () => {
+                eventManager.eventText.text = $"You chose to leave the item for another adventurer to find.";
+                eventManager.itemRewardPanel.SetActive(false);
+            },
         };
 
         return newEvent;
